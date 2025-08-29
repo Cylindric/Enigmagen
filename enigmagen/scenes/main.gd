424309161f7d3fe68508c01e505abcb81e64eead
Cylindric <mark@hanfordonline.co.uk>
2025-08-29 20:11:24 +0100

    Faffing

--- enigmagen/scenes/main.gd
+++ enigmagen/scenes/main.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var surface: TileMapLayer = %Surface
 
+enum TERRAIN {GROUND1, GROUND2, ROCK, LANDER}
+
 var rng = RandomNumberGenerator.new()
 
 func _ready():
@@ -16,11 +18,17 @@ func _init_grid() -> void:
 
 	@warning_ignore("integer_division")
 	var rows := screen_size.y / 64
-	
+
 	print("Screen size is ", screen_size, " @ ", DisplayServer.screen_get_scale())
 	print("Creating map with ", columns, " columns and ", rows, " rows.")
-	
+
 	for col in columns:
 		for row in rows:
-			var tile := rng.randi_range(0, 1)
-			surface.set_cell(Vector2(col, row), 0, Vector2(tile, 0))
+			var tile_id: int
+
+			if col == columns/2 && row == rows/2:
+				tile_id = TERRAIN.LANDER
+			else:
+				tile_id = rng.randi_range(TERRAIN.GROUND1, TERRAIN.GROUND2)
+
+			surface.set_cell(Vector2(col, row), 0, Vector2(tile_id, 0))
